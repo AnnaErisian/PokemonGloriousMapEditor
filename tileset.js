ac1b1910802870b4c96f0830d97d32a60548817d
Jacob Inkrote <jaketheultimate@gmail.com>
2016-10-02 13:15:37 -0400

    Implemented selection.  Still figuring out how renders work

--- tileset.js
+++ tileset.js
@@ -1,8 +1,9 @@
-function createTileset(id, image) {
+function createTilesetModule(idNum, image) {
   return {
-    module: null;
-    id: 0,
-    tileset: null,
+    module: null,
+    canvas: null,
+    id: idNum,
+    tileset: image,
     currentSelection: null,
     startCoordinates: [-1,-1],
     
@@ -18,24 +19,36 @@ function createTileset(id, image) {
     
     startSetSelection: function(event) {
       //set startCoords to current coords (32px squares, we want 1-8th square number)
+      var coords = getCursorPosition(canvas, event);
+      startCoordinates[0] = coords.x/32;
+      startCoordinates[1] = coords.y/32;
     },
     
     finishSetSelection: function(event) {
-      //if start is -1 -1, null current and print error
+      //if start is -1 -1, leave current and print error
+      if(startCoordinates[0] == -1 && startCoordinates[1] == -1) {
+        console.log("finished Set Selection flow without starting coordinates");
+        return;
+      }
       //otherwise, calculate current selection and reset start coords
+      var coords = getCursorPosition(canvas, event);
+      coords.x = coords.x/32;
+      coords.y = coords.y/32;
+      currentSelection = {
+        x: coords.x,
+        y: coords.y,
+        w: coords.x - startCoordinates[0],
+        h: coords.y - startCoordinates[1],
+      };
+      startCoordinates = [-1, -1];
     },
+    
     terminateSetSelection: function(event) {
       startCoordinates = [-1, -1];
     },
-    setTileset: function(data) {
-      tileset = data;
-    },
+    
     render: function() {
       //create html
-      //call parent's render
-    },
-    createModule: function() {
-      //set data
     }
   };
 }

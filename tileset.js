4db5a0c60cf4bba914b4e149649460742e46f2b5
Jacob Inkrote <jaketheultimate@gmail.com>
2016-10-05 12:05:03 -0400

    fixed naming issue

--- tileset.js
+++ tileset.js
@@ -1,7 +1,7 @@
 function createTilesetModule(idNum, image, iconimage) {
   return {
     tabTemplate: $("#tabTemplate")[0].innerHTML,
-    tilesetTemplate: $("#tilesheetTemplate")[0].innerHTML,
+    tilesheetTemplate: $("#tilesheetTemplate")[0].innerHTML,
     module: null,
     canvas: null,
     id: idNum,
@@ -54,6 +54,7 @@ function createTilesetModule(idNum, image, iconimage) {
       console.log("Tileset Render");
       
       //create html
+      console.log(tabTemplate);
       parent.tabUL.innerHTML += Mustache.render(tabTemplate, {iconImg: this.icon, n: this.id});
       parent.module.innerHTML += Mustache.render(tilesheetTemplate, {n: this.id});
       this.module = {tabHead: parent.module.find("#tabHead-"+id), tab: parent.module.find("tabs-"+id)};

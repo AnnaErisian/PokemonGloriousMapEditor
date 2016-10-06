d5ebacec89e46d9439dc0b0726983f58c56a0d1c
Jacob Inkrote <jaketheultimate@gmail.com>
2016-10-05 23:33:16 -0400

    toString is useless.

--- tileset.js
+++ tileset.js
@@ -58,6 +58,8 @@ function createTilesetModule(idNum, image, iconimage) {
       console.log(tilesheetTemplate);
       console.log(tabTemplate.toString());
       console.log(tilesheetTemplate.toString());
+      console.log(""+tabTemplate);
+      console.log(""+tilesheetTemplate);
       console.log(typeof tabTemplate);
       console.log(typeof tilesheetTemplate);
       var renderedTab = Mustache.render(tabTemplate, {iconImg: this.icon, n: this.id});

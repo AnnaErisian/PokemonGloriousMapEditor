a12ca266c9e982634fe55eee19e052bd8dad82d2
sigmaJ <jaketheultimate@gmail.com>
2016-10-10 16:37:50 -0400

    Added layer and management modules, scripts added to page

---
+++ layer.js
@@ -0,0 +1,26 @@
+var mapModule = {
+  module: null,
+  activeLayer: null,
+  setActiveLayer: function(newLayerNumber) {
+    this.activeLayer = newLayerNumber;
+    console.log('changed active layer: ' + newLayerNumber);
+  },
+  createModule() {
+    console.log("Layer Module Creation");
+  },
+  render: function() {
+	console.log("Layer Module Render");
+    //create html
+    this.module = $("#layerModule")[0];
+    for(i = 0; i < 5; i++) {
+      module.append('<span class="layerButton" id="layerButton-' + i + '" ><img src="layerImage-' + i + '" alt="layer-' + i + '" /></span>');
+    }
+  },
+  registerEvents: function() {
+    var buttons = this.module.find(".layerButton");
+    
+    for(i = 0; i < 5; i++) {
+      buttons[i].addEventListener("click", (function() {this.setActiveLayer(i);}).bind(this));
+    }
+  }
+};
---
+++ management.js
@@ -0,0 +1,14 @@
+var mapModule = {
+  module: null,
+  
+  createModule() {
+    console.log("Management Module Creation");
+  },
+  render: function() {
+	console.log("Management Module Render");
+    //nothing yet
+  },
+  registerEvents: function() {
+    //nothing yet
+  }
+};

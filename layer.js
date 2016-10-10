56df150d3505fe17b080084f04b85d0faeefc73e
Jacob Inkrote <jaketheultimate@gmail.com>
2016-10-10 18:37:41 -0400

    style oddities

--- layer.js
+++ layer.js
@@ -6,6 +6,10 @@ var layerModule = {
     this.activeLayer = newLayerNumber;
     console.log('changed active layer: ' + newLayerNumber);
   },
+  glowActiveLayer: function() {
+    var buttons = this.module.find(".layerButton");
+    buttons.css('box-shadow', 'none');
+  },
   createModule() {
     console.log("Layer Module Creation");
 	this.buttonTemplate = $("#layerButtonTemplate")[0];
@@ -23,20 +27,23 @@ var layerModule = {
     
     buttons[0].addEventListener("click", function() {
       layerModule.setActiveLayer(0); //passing is wierd.
-      console.log(this);
+      layerModule.glowActiveLayer();
     });
     buttons[1].addEventListener("click", function() {
       layerModule.setActiveLayer(1); //passing is wierd.
-      this.setAttribute("box-shadow", "0px 0px 5px white");
+      layerModule.glowActiveLayer();
     });
-    buttons[2].addEventListener("click", (function() {
-      this.setActiveLayer(2); //passing is wierd.
-    }).bind(this));
-    buttons[3].addEventListener("click", (function() {
-      this.setActiveLayer(3); //passing is wierd.
-    }).bind(this));
-    buttons[4].addEventListener("click", (function() {
-      this.setActiveLayer(4); //passing is wierd.
-    }).bind(this));
+    buttons[2].addEventListener("click", function() {
+      layerModule.setActiveLayer(2); //passing is wierd.
+      layerModule.glowActiveLayer();
+    });
+    buttons[3].addEventListener("click", function() {
+      layerModule.setActiveLayer(3); //passing is wierd.
+      layerModule.glowActiveLayer();
+    });
+    buttons[4].addEventListener("click", function() {
+      layerModule.setActiveLayer(4); //passing is wierd.
+      layerModule.glowActiveLayer();
+    });
   }
 };
